9fc96c8eeafb80acb724cf05c6343eac0fe2515d
torabit <pujigami@gmail.com>
2023-05-01 20:18:41 +0900

    Add Client

---
+++ itunes.go
@@ -0,0 +1,153 @@
+package itunes
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	// defaultRetryDurationS helps us fix an apparent server bug whereby we will
+	// be told to retry but not be given a wait-interval.
+	defaultRetryDuration = time.Second * 5
+
+	// rateLimitExceededStatusCode is the code that the server returns when our
+	// request frequency is too high.
+	rateLimitExceededStatusCode = 429
+)
+
+type Client struct {
+	http    *http.Client
+	baseURL string
+
+	autoRetry bool
+}
+
+type ClientOption func(client *Client)
+
+func New(httpClient *http.Client, opts ...ClientOption) *Client {
+	c := &Client{
+		http:    httpClient,
+		baseURL: "https://itunes.apple.com/",
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
+}
+
+// Error represents an error returned by the Spotify Web API.
+type Error struct {
+	// A short description of the error.
+	Message string `json:"message"`
+	// The HTTP status code.
+	Status int `json:"status"`
+}
+
+func (e Error) Error() string {
+	return e.Message
+}
+
+// decodeError decodes an Error from an io.Reader.
+func (c *Client) decodeError(resp *http.Response) error {
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if len(responseBody) == 0 {
+		return fmt.Errorf("spotify: HTTP %d: %s (body empty)", resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	buf := bytes.NewBuffer(responseBody)
+
+	var e struct {
+		E Error `json:"error"`
+	}
+	err = json.NewDecoder(buf).Decode(&e)
+	if err != nil {
+		return fmt.Errorf("spotify: couldn't decode error: (%d) [%s]", len(responseBody), responseBody)
+	}
+
+	if e.E.Message == "" {
+		// Some errors will result in there being a useful status-code but an
+		// empty message, which will confuse the user (who only has access to
+		// the message and not the code). An example of this is when we send
+		// some of the arguments directly in the HTTP query and the URL ends-up
+		// being too long.
+
+		e.E.Message = fmt.Sprintf("spotify: unexpected HTTP %d: %s (empty error)",
+			resp.StatusCode, http.StatusText(resp.StatusCode))
+	}
+
+	return e.E
+}
+
+func retryDuration(resp *http.Response) time.Duration {
+	raw := resp.Header.Get("Retry-After")
+	if raw == "" {
+		return defaultRetryDuration
+	}
+	seconds, err := strconv.ParseInt(raw, 10, 32)
+	if err != nil {
+		return defaultRetryDuration
+	}
+	return time.Duration(seconds) * time.Second
+}
+
+// shouldRetry determines whether the status code indicates that the
+// previous operation should be retried at a later time
+func shouldRetry(status int) bool {
+	return status == http.StatusAccepted || status == http.StatusTooManyRequests
+}
+
+func (c *Client) get(ctx context.Context, url string, result interface{}) error {
+	for {
+		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+
+		if err != nil {
+			return err
+		}
+
+		resp, err := c.http.Do(req)
+
+		if err != nil {
+			return err
+		}
+
+		defer resp.Body.Close()
+
+		if resp.StatusCode == rateLimitExceededStatusCode && c.autoRetry {
+			select {
+			case <-ctx.Done():
+			// If the context is cancelled, return the original error
+			case <-time.After(retryDuration(resp)):
+				continue
+			}
+		}
+
+		if resp.StatusCode == http.StatusNoContent {
+			return nil
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return c.decodeError(resp)
+		}
+
+		err = json.NewDecoder(resp.Body).Decode(result)
+		if err != nil {
+			return err
+		}
+
+		break
+	}
+
+	return nil
+}
