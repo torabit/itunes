7e34786ae37e2becb17ee9c229a0b9df7cfd45b6
torabit <pujigami@gmail.com>
2023-05-01 17:08:39 +0900

    Remove types

---
+++ lang.go
@@ -0,0 +1,12 @@
+package itunes
+
+// The language, English or Japanese,
+// you want to use when returning search results.
+// Specify the language using the five-letter codename.
+// For example: en_us.
+// The default is en_us (English).
+
+const (
+	LangEnglish  = "en_us"
+	LangJapanese = "ja_jp"
+)
--- media.go
+++ media.go
@@ -1,16 +1,14 @@
 package itunes
 
-type MediaType string
-
 const (
-	MediaMovie      MediaType = "movie"      // movieArtist, movie
-	MediaPodcast              = "podcast"    // podcastAuthor, podcast
-	MediaMusic                = "music"      // musicArtist, musicTrack, album, musicVideo, mix, song.
-	MediaMusicVideo           = "musicVideo" // musicArtist, musicVideo
-	MediaAudioBook            = "audiobook"  // audiobookAuthor, audiobook
-	MediaShortFilm            = "shortFilm"  // shortFilmArtist, shortFilm
-	MediaTvShow               = "tvShow"     // tvEpisode, tvSeason
-	MediaSoftware             = "software"   // software, iPadSoftware, macSoftware
-	MediaEbook                = "ebook"      // ebook
-	MediaAll                  = "all"        // movie, album, allArtist, podcast, musicVideo, mix, audiobook, tvSeason, allTrack
+	MediaMovie      = "movie"      // movieArtist, movie
+	MediaPodcast    = "podcast"    // podcastAuthor, podcast
+	MediaMusic      = "music"      // musicArtist, musicTrack, album, musicVideo, mix, song.
+	MediaMusicVideo = "musicVideo" // musicArtist, musicVideo
+	MediaAudioBook  = "audiobook"  // audiobookAuthor, audiobook
+	MediaShortFilm  = "shortFilm"  // shortFilmArtist, shortFilm
+	MediaTvShow     = "tvShow"     // tvEpisode, tvSeason
+	MediaSoftware   = "software"   // software, iPadSoftware, macSoftware
+	MediaEbook      = "ebook"      // ebook
+	MediaAll        = "all"        // movie, album, allArtist, podcast, musicVideo, mix, audiobook, tvSeason, allTrack
 )
