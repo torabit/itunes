c8a03d6837e40560ab2c8fba5a8b8e831c847bae
torabit <pujigami@gmail.com>
2023-05-01 17:19:55 +0900

    Remove description

--- lang.go
+++ lang.go
@@ -1,11 +1,5 @@
 package itunes
 
-// The language, English or Japanese,
-// you want to use when returning search results.
-// Specify the language using the five-letter codename.
-// For example: en_us.
-// The default is en_us (English).
-
 const (
 	LangEnglish  = "en_us"
 	LangJapanese = "ja_jp"
