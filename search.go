54b7278a8e17eb1ab0516f2c782a24970b374848
torabit <pujigami@gmail.com>
2023-05-01 20:18:47 +0900

    Add Search Api

---
+++ search.go
@@ -0,0 +1,19 @@
+package itunes
+
+import "context"
+
+func (c *Client) Search(ctx context.Context, opts ...RequestOption) (*iTunesResult, error) {
+	v := processOptions(opts...).urlParams
+
+	itunesUrl := c.baseURL + "search?" + v.Encode()
+
+	var result iTunesResult
+
+	err := c.get(itunesUrl, &result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, err
+}
