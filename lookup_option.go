ad4beefdf2cc663deae63446a031f854b8327aa2
torabit <pujigami@gmail.com>
2023-05-16 01:27:32 +0900

    Add lookup option

---
+++ lookup_option.go
@@ -0,0 +1,54 @@
+package itunes
+
+import (
+	"net/url"
+	"strconv"
+)
+
+// Lookup Options
+type LookupOption func(*requestOption)
+
+func ID(id int64) LookupOption {
+	return func(o *requestOption) {
+		o.urlParams.Set("id", strconv.FormatInt(id, 10))
+	}
+}
+
+func AMGArtistID(amgArtistId int64) LookupOption {
+	return func(o *requestOption) {
+		o.urlParams.Set("amgArtistId", strconv.FormatInt(amgArtistId, 10))
+	}
+}
+
+func AMGAlbumID(amgAlbumId int64) LookupOption {
+	return func(o *requestOption) {
+		o.urlParams.Set("amgAlbumId", strconv.FormatInt(amgAlbumId, 10))
+	}
+}
+
+func AMGVideoID(amgVideoId int64) LookupOption {
+	return func(o *requestOption) {
+		o.urlParams.Set("amgVideoId", strconv.FormatInt(amgVideoId, 10))
+	}
+}
+
+func UPC(upc int64) LookupOption {
+	return func(o *requestOption) {
+		o.urlParams.Set("upc", strconv.FormatInt(upc, 10))
+	}
+}
+
+func ISBN(isbn int64) LookupOption {
+	return func(o *requestOption) {
+		o.urlParams.Set("isbn", strconv.FormatInt(isbn, 10))
+	}
+}
+
+func processLookupOption(option LookupOption) requestOption {
+	o := requestOption{
+		urlParams: url.Values{},
+	}
+	option(&o)
+
+	return o
+}
