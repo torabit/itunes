80ae037e7f4f5bc43cf93d5f8acffc0a9ff81997
torabit <pujigami@gmail.com>
2023-07-02 14:35:50 +0900

    Add releaseDate and isStreamable property

--- result.go
+++ result.go
@@ -1,5 +1,7 @@
 package itunes
 
+import "time"
+
 // iTunesResult represent iTunes response outer most structure
 type iTunesResult struct {
 	ResultCount int      `json:"resultCount"`
@@ -7,32 +9,35 @@ type iTunesResult struct {
 }
 
 type Result struct {
-	WrapperType            string  `json:"wrapperType"`
-	Kind                   string  `json:"kind"`
-	ArtistId               int64   `json:"artistId"`
-	CollectionId           int64   `json:"collectionId"`
-	TrackId                int64   `json:"trackId"`
-	ArtistName             string  `json:"artistName"`
-	CollectionName         string  `json:"collectionName"`
-	TrackName              string  `json:"trackName"`
-	CollectionCensoredName string  `json:"collectionCensoredName"`
-	TrackCensoredName      string  `json:"trackCensoredName"`
-	ArtistViewUrl          string  `json:"artistViewUrl"`
-	CollectionViewUrl      string  `json:"collectionViewUrl"`
-	TrackViewUrl           string  `json:"trackViewUrl"`
-	PreviewUrl             string  `json:"previewUrl"`
-	ArtworkUrl60           string  `json:"artworkUrl60"`
-	ArtworkUrl100          string  `json:"artworkUrl100"`
-	CollectionPrice        float64 `json:"collectionPrice"`
-	TrackPrice             float64 `json:"trackPrice"`
-	CollectionExplicitness string  `json:"collectionExplicitness"`
-	TrackExplicitness      string  `json:"trackExplicitness"`
-	DiscCount              int64   `json:"discCount"`
-	DiscNumber             int64   `json:"discNumber"`
-	TrackCount             int64   `json:"trackCount"`
-	TrackNumber            int64   `json:"trackNumber"`
-	TrackTimeMillis        int64   `json:"trackTimeMillis"`
-	Country                string  `json:"country"`
-	Currency               string  `json:"currency"`
-	PrimaryGenreName       string  `json:"primaryGenreName"`
+	WrapperType            string    `json:"wrapperType"`
+	Kind                   string    `json:"kind"`
+	ArtistId               int64     `json:"artistId"`
+	CollectionId           int64     `json:"collectionId"`
+	TrackId                int64     `json:"trackId"`
+	ArtistName             string    `json:"artistName"`
+	CollectionName         string    `json:"collectionName"`
+	TrackName              string    `json:"trackName"`
+	CollectionCensoredName string    `json:"collectionCensoredName"`
+	TrackCensoredName      string    `json:"trackCensoredName"`
+	ArtistViewUrl          string    `json:"artistViewUrl"`
+	CollectionViewUrl      string    `json:"collectionViewUrl"`
+	TrackViewUrl           string    `json:"trackViewUrl"`
+	PreviewUrl             string    `json:"previewUrl"`
+	ArtworkUrl30           string    `json:"artworkUrl30"`
+	ArtworkUrl60           string    `json:"artworkUrl60"`
+	ArtworkUrl100          string    `json:"artworkUrl100"`
+	CollectionPrice        float64   `json:"collectionPrice"`
+	TrackPrice             float64   `json:"trackPrice"`
+	ReleaseDate            time.Time `json:"releaseDate"`
+	CollectionExplicitness string    `json:"collectionExplicitness"`
+	TrackExplicitness      string    `json:"trackExplicitness"`
+	DiscCount              int64     `json:"discCount"`
+	DiscNumber             int64     `json:"discNumber"`
+	TrackCount             int64     `json:"trackCount"`
+	TrackNumber            int64     `json:"trackNumber"`
+	TrackTimeMillis        int64     `json:"trackTimeMillis"`
+	Country                string    `json:"country"`
+	Currency               string    `json:"currency"`
+	PrimaryGenreName       string    `json:"primaryGenreName"`
+	IsStreamable           bool      `json:"isStreamable"`
 }
